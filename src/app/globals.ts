4f5a5d56873401520c4389fb6883fcb04b5b54ed
npatrick96 <patrickniyongabo@gmail.com>
2017-07-20 13:44:56 -0400

    changed backend IP address

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,5 +1,7 @@
 'use strict';
 
 //export const baseUrl='http://localhost:8080/api/';
-export const baseUrl='http://54.227.187.154:8080/api/';
+//export const baseUrl='http://54.209.199.209:8080/api/';
+
+export const baseUrl = 'http://54.227.187.154:8080/api/';
 
