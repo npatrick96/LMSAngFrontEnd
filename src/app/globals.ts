cd35a546a9eb89cba23ad7b6f055aee88e4c26da
npatrick96 <patrickniyongabo@gmail.com>
2017-07-28 16:42:25 -0400

    it friYAY

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,6 +1,4 @@
 'use strict';
 
-//export const baseUrl='http://localhost:8080/api/';
-export const baseUrl='http://10.0.2.164:8080/api/';
-//export const baseUrl = 'http://54.158.249.41:8080/api/';
+export const baseUrl="http://10.0.2.164:8080/api/";
 
